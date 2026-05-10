8fa7541affd8287fb74f9c8ec38cabbcc6193e61
vegecloud <caiyun_yap@hotmail.com>
2026-05-10 16:27:17 +0800

    feat: tic-tac-toe game

--- project-tic-tac-toe/src/App.jsx
+++ project-tic-tac-toe/src/App.jsx
@@ -3,10 +3,49 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  const [player, setPlayers] = useState({ 'X': 'Player 1', 'O': 'Player 2' });
+
   const activePlayer = deriveActivePlayer(gameTurns);
+  let gameBoard = [...initialGameBoard.map(arr => [...arr])]; // deep copy of gameboard
+  let winner;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].col];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = player[firstSquareSymbol];
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  const handlePlayerNameChange = (symbol, newName) => {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName // setting propery of object representing player
+      }
+    });
+  };
 
   const handleSelectSquare = (rowIndex, colIndex) => {
     setGameTurns(prevTurns => {
@@ -19,6 +58,10 @@ function App() {
     });
   }
 
+  const handleRematch = () => {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -27,16 +70,19 @@ function App() {
             name="Player 1"
             symbol="X"
             isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             name="Player 2"
             symbol="O"
             isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
+        {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch} />}
         <GameBoard
           onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
+          board={gameBoard}
         />
       </div>
       <Log turns={gameTurns} />
@@ -57,4 +103,8 @@ export default App
 /** Note:
  *  We can lift states up to the closest ancestor component that has 
  *  access to all the components that need to work with that state.
+ * 
+ *  When amending referenced values directly (e.g. objects, arrays),
+ *  we should note to create a deep copy of the referenced values
+ *  so that the values of the original copy do not get affected.
  */
--- project-tic-tac-toe/src/components/Player.jsx
+++ project-tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol, isActive }) {
+export default function Player({ name, symbol, isActive, onChangeName }) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(name);
 
   const handleClick = () => {
     setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName)
+    }
   }
 
   const handleChange = (event) => {
